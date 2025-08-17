bfc408ecea105e865126bd8574f2242e805c4d09
yLu4n <lu.victor.2005@gmail.com>
2025-08-17 17:49:31 -0300

    Nova view para mostrar os tópicos presentes no banco de dados

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -19,5 +19,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    
+    path('topics/', views.topics, name='topics'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 def index(request):
     """Página principal"""
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Mostrar todos os temas"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
