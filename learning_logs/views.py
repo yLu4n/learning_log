9d7caa9d450e66607a7218a2264f792573f58263
yLu4n <lu.victor.2005@gmail.com>
2025-08-20 17:35:57 -0300

    Adicionando as views das entradas

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -11,3 +11,10 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
+
+def topic(request,topic_id):
+    """Mostrar um único tema e todas as suas entradas"""
+    topic = Topic.objects.get(id= topic_id)
+    entrys = topic.entry_set.order_by('-date_added') # Ordenando as entradas do mais antigo para o mais recente
+    context = {'topic': topic, 'entrys': entrys}
+    return render(request, 'learning_logs/topic.html', context)
