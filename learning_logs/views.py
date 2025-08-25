c84653992bf5de8b3886d1afd6408edc18e943ee
yLu4n <lu.victor.2005@gmail.com>
2025-08-25 17:41:47 -0300

    Criação do arquivo para criação de formulários

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+
 from .models import Topic
+from .forms import TopicForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -18,3 +22,15 @@ def topic(request,topic_id):
     entrys = topic.entry_set.order_by('-date_added') # Ordenando as entradas do mais antigo para o mais recente
     context = {'topic': topic, 'entrys': entrys}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Adicionar novo topico"""
+    if request.method != 'POST':
+        form = TopicForm() #Cria um formulário vazio
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topics')) # Redireciona para a página de tópicos
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
